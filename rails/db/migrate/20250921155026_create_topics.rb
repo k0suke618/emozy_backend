a33561a352166f74e3c5ea56c0599ad233bba158
K <23rd064@ms.dendai.ac.jp>
2025-09-23 00:48:21 +0900

    fix topic

--- rails/db/migrate/20250921155026_create_topics.rb
+++ rails/db/migrate/20250921155026_create_topics.rb
@@ -1,7 +1,7 @@
 class CreateTopics < ActiveRecord::Migration[7.2]
   def change
     create_table :topics do |t|
-      t.text :topic, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
