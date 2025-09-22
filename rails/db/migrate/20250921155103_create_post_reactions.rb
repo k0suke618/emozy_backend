29a086708762a0e52b97ff0b4e6ddce0afaccf0c
K <23rd064@ms.dendai.ac.jp>
2025-09-23 00:44:28 +0900

    create migrate file (follows, posts, reactions, topics, post_reactions)

--- rails/db/migrate/20250921155103_create_post_reactions.rb
+++ rails/db/migrate/20250921155103_create_post_reactions.rb
@@ -1,6 +1,10 @@
 class CreatePostReactions < ActiveRecord::Migration[7.2]
   def change
     create_table :post_reactions do |t|
+      t.references :post, null: false, foreign_key: true
+      t.references :topic, null: false, foreign_key: true
+      t.references :reaction, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
