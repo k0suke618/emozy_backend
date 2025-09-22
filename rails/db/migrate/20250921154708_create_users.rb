4fdbb10e0d49728f2b0919a13d0ad4332d52b76d
yura <yduara2248@gmail.com>
2025-09-23 00:06:10 +0900

    usersについて追加

--- rails/db/migrate/20250921154708_create_users.rb
+++ rails/db/migrate/20250921154708_create_users.rb
@@ -1,7 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false, index: { unique: true }
+      t.text :profile, null: true
+      t.bigint :point, null: false, default: 0
+      t.references :icon, foreign_key: { to_table: :icons }, null: false, default: 1
+      t.references :background, foreign_key: { to_table: :backgrounds }, null: true
+      t.references :frame, foreign_key: { to_table: :frames }, null: true
+
+      t.string :encrypted_password, null: false, default: ""
+
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
