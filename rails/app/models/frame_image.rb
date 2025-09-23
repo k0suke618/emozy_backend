02034d97af00819185ae6f866f8f12f36c4685ae
yura <yduara2248@gmail.com>
2025-09-23 22:05:10 +0900

    feat: User,background,frameについて追加

--- rails/app/models/frame_image.rb
+++ rails/app/models/frame_image.rb
@@ -1,2 +1,10 @@
 class FrameImage < ApplicationRecord
+  # アソシエーション
+  has_many :users, foreign_key: 'frame_id'
+  has_many :frame_lists, dependent: :destroy
+  has_many :users_owning_this, through: :frame_lists, source: :user
+
+  # バリデーション
+  validates :image, presence: true
+  validates :point, presence: true
 end
--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  # アソシエーション
+  belongs_to :background, class_name: 'BackgroundImage', optional: true
+  belongs_to :frame, class_name: 'FrameImage', optional: true
+  has_many :background_lists, dependent: :destroy
+  has_many :background_images, through: :background_lists, source: :image
+  has_many :frame_lists, dependent: :destroy
+  has_many :frame_images, through: :frame_lists, source: :image
+
+  # バリデーション
+  validates :name, presence: true
+  validates :point, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # パスワードのハッシュ化と認証
+  has_secure_password
 end
