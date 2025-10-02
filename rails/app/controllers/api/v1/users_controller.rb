c11d6c7fc4133fa25d69e1b36304159a754eac62
K <23rd064@ms.dendai.ac.jp>
2025-10-03 00:22:00 +0900

    add: アイコン用のkeyを追加

--- rails/app/controllers/api/v1/users_controller.rb
+++ rails/app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,8 @@ module Api
       # GET /api/v1/users/:id
       def show
         user = User.find(params[:id])
-        render json: user
+        # render json: user
+        render json: serialize_user(user)
       end
 
       # PATCH /api/v1/users/:id
@@ -23,6 +24,15 @@ module Api
       private
       def user_params
         params.require(:user).permit(:email, :password)
+      end
+
+      # TODO: あとで、画像URLを追加する
+      def serialize_user(user)
+        user.as_json().merge(
+          icon_image_url: "",
+          background_image_url: "",
+          frame_image_url: ""
+        )
       end
     end
   end
