1486a01c6073778d1af805ce1d6208bf8c95b4a2
K <23rd064@ms.dendai.ac.jp>
2025-10-03 21:32:18 +0900

    fix: icon_parts_controllerのグルーピングを修正

--- rails/app/controllers/api/v1/icon_parts_controller.rb
+++ rails/app/controllers/api/v1/icon_parts_controller.rb
@@ -3,8 +3,12 @@ module Api
     class IconPartsController < ApplicationController
       # アイコンパーツの取得
       def index
-        icon_parts = IconPart.all.group_by(&:part_type)
-        render json: icon_parts, status: :ok
+        icon_parts = IconPart.includes(:icon_parts_type)
+        grouped_parts = icon_parts.group_by { |part| part.icon_parts_type&.content }
+
+        render json: {
+          icon_parts: grouped_parts.transform_values { |parts| parts.map { |part| part.as_json } }
+        }, status: :ok
       end
     end
   end
